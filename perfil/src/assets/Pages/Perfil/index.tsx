672ffd2b6e3b0ae184b636f1576d6f6e7b81aaf8
Fernanda-Cidade <fernanda.cidade@outlook.com>
2023-09-04 21:45:57 -0300

    Inicio da Tabela

--- perfil/src/assets/Pages/Perfil/index.tsx
+++ perfil/src/assets/Pages/Perfil/index.tsx
@@ -1,10 +1,43 @@
-import "./style.css";
+import * as React from 'react';
+import { useState } from 'react';
+// import './index.css'
+// import fakeData from './'; Aqui vamos importar a API 
 
-function PerfilUsuario(){
 
-    return (
+
+function Perfil(){
+    function informacaoUsuario (){
+        const [usuers, Setusuers] = useState<string[]>(
+            matricula: 1231720,
+            nome: Thiago Nascimento,
+            area: Chao de Fabrica,
+            dataNascimento: 28/08/1998,
+            funcao: Chefe chao de fabrica,
+            sessao: 304,
+            id: 1
+        );
         
+    }
+    return(
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Matricula</th>
+                        <th>Nome</th>
+                        <th>Area</th>
+                        <th>Data de Nascimento</th>
+                        <th>Função</th>
+                        <th>Sessão</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    
     );
-}
-  
-export default PerfilUsuario;
+}export default Perfil;

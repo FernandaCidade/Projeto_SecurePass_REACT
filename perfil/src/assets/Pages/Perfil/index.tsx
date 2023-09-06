4992ca213f9cd33790910bdcadd96063019444f3
Fernanda-Cidade <fernanda.cidade@outlook.com>
2023-09-05 21:47:27 -0300

    Inclementação da tabela e Css

--- perfil/src/assets/Pages/Perfil/index.tsx
+++ perfil/src/assets/Pages/Perfil/index.tsx
@@ -1,43 +1,77 @@
 import * as React from 'react';
 import { useState } from 'react';
-// import './index.css'
+import "./style.css"
 // import fakeData from './'; Aqui vamos importar a API 
 
 
 
 function Perfil(){
+
     function informacaoUsuario (){
-        const [usuers, Setusuers] = useState<string[]>(
-            matricula: 1231720,
-            nome: Thiago Nascimento,
-            area: Chao de Fabrica,
-            dataNascimento: 28/08/1998,
-            funcao: Chefe chao de fabrica,
+
+       
+        
+        const [users, Setusers] = useState<any[]>([
+            {
+            img_perfil: "Foto_perfil.png",
+            nome: "Thiago Nascimento",
+            area: "Chao de Fabrica",
+            dataNascimento: "28/08/1998",
+            funcao: "Chefe chao de fabrica",
             sessao: 304,
             id: 1
-        );
-        
+            }
+        ])
+    
     }
+
+    const colunas = [
+
+        {
+            title: 'Nome',
+            field: 'nome'
+        },
+
+        {
+            title: 'Area',
+            field: 'area'
+        },
+        {
+            title: 'Data de Nascimento',
+            field: 'dataNascimento'
+        },
+        {
+            title: 'Função',
+            field: 'funcao'
+        },
+        {
+            title: 'Sessão',
+            field: 'sessao'
+        },
+       
+    ]
+
+
     return(
         <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Matricula</th>
-                        <th>Nome</th>
-                        <th>Area</th>
-                        <th>Data de Nascimento</th>
-                        <th>Função</th>
-                        <th>Sessão</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    
+            <div className='emglobador'>
+                <span>#ADM</span>
+                <table className='tabelaPerfil'>
+                    <thead className='esquerda'>
+                        <tr>
+                            <th>Matricula</th>
+                            <th>Nome</th>
+                            <th>Area</th>
+                            <th>Data de Nascimento</th>
+                            <th>Função</th>
+                            <th>Sessão</th>
+                        </tr>
+                    </thead>
+                    <tbody className='direita'>
+                        <td> {users} </td>
+                    </tbody>
+                </table>
+            </div>
+    </div>
     );
 }export default Perfil;
